258be8be7244e980b62edcf59fd7917f413135cc
lichen <1960003945@qq.com>
2018-10-11 10:37:23 +0800

    commit

---
+++ src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -0,0 +1,22 @@
+package com.example.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+// 发现和注册服务
+//@EnableDiscoveryClient
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+
+
+}
